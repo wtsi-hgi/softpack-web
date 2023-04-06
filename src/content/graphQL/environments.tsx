ba1fe0015faf3db3c35f6253b2ce387e72ac27b2
KMace <kyle.mace@hotmail.co.uk>
2023-04-06 08:14:43 +0100

    Error Messages

--- src/content/graphQL/environments.tsx
+++ src/content/graphQL/environments.tsx
@@ -2,6 +2,7 @@ import { Box, Typography } from "@mui/material"
 
 import { ALL_ENVIRONMENTS } from "./queries"
 import { useQuery } from "@apollo/client"
+import { useState } from "react"
 
 const Owner = (owner: any) => {
   return (
@@ -24,7 +25,15 @@ const Package = (pckg: any) => {
 }
 
 const Environments = (props: { show: boolean }) => {
-  const result = useQuery(ALL_ENVIRONMENTS)
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  const result = useQuery(ALL_ENVIRONMENTS, {
+    onError: (error) => {
+      console.log(error)
+      const message = error.graphQLErrors[0].message
+      setErrorMessage(message)
+    }
+  })
   console.log(result)
 
   if (result.loading) {
@@ -33,6 +42,14 @@ const Environments = (props: { show: boolean }) => {
 
   if (!props.show) {
     return null
+  }
+
+  if (result.error) {
+    return (
+      <div style={{color:'red'}}>
+        {result.error.message}
+      </div>
+    )
   }
   
   return (
