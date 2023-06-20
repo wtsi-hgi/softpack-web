9e54d5b97aa8e4f4d876ca5e41c1bb8c2ac6c815
KMace <kyle.mace@hotmail.co.uk>
2023-06-20 12:45:08 +0100

    React Decorated Fetching of Env Data

--- src/content/graphQL/CreateEnvironment/envSelect.tsx
+++ src/content/graphQL/CreateEnvironment/envSelect.tsx
@@ -1,25 +1,58 @@
 import { useQuery } from "@apollo/client";
 import { TabContext, TabList, TabPanel } from "@mui/lab";
-import { Autocomplete, Box, Tab, TextField } from "@mui/material";
+import { Autocomplete, Box, Divider, Tab, TextField, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
+
+interface Environment {
+  description: string;
+  id: string;
+  name: string;
+  packages: Package[];
+  path: string;
+  __typename: string;
+}
+
+interface Package {
+  id: string;
+  name: string;
+  version: string;
+}
 
 function EnvSelect(data) {
   const [value, setValue] = useState(null);
-  const [pythonPackages, setPythonPackages] = useState([]);
+  const [environments, setEnvironments] = useState<Environment[]>([]);
 
   useEffect(() => {
-    //const namesArray = data.packages.packages.map(item => item.name);
-    console.log('data', data.data)
-    //setPythonPackages(namesArray)
-    
+    const random = [data.data]
+    setEnvironments(random);
+
     const handleChange = (event: React.SyntheticEvent, newValue: string) => {
       setValue(newValue);
     }
-  })
+  }, [])
   
   return (
     <Box> 
-      Hello
+      <Typography variant="h2">Environments:</Typography >
+      <Divider/>
+    
+      {environments.map((env) => {
+        return (
+          <Box key={env.id}>
+            <Typography><Typography variant={"h4"}>Name:</Typography> {env.name}</Typography>
+            <Typography><Typography variant={"h4"}>Description:</Typography> {env.description}</Typography>            
+            {env.packages.map((package_) => {
+              return (
+                <Box key={package_.id}>
+                  <Typography>Package Name: {package_.name}</Typography>
+                  {package_.version && <Typography>Package Version: {package_.version}</Typography>}
+                </Box>
+              );
+            })}
+            <Typography><Typography variant={"h4"}>Path:</Typography> {env.path}</Typography>
+          </Box>
+        );
+      })}
     </Box>
   );
 }
