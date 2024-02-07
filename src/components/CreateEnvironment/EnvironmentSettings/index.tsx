34ae31005aec03ab175f97aa0c4bf901224e1623
ash <ah37@sanger.ac.uk>
2024-02-07 13:36:42 +0000

    Move username entry to top-right of page

--- src/components/CreateEnvironment/EnvironmentSettings/index.tsx
+++ src/components/CreateEnvironment/EnvironmentSettings/index.tsx
@@ -14,9 +14,10 @@ import {
 } from "@mui/material";
 
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
-import { useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { useQuery } from "@apollo/client";
 import { GROUPS } from "../../../queries";
+import { UsernameContext } from "../../UsernameContext";
 
 type EnvironmentSettingsProps = {
   name: string;
@@ -31,7 +32,7 @@ type EnvironmentSettingsProps = {
 // available to a user when creating a new environment. E.g. Name, Description,
 // etc.
 function EnvironmentSettings(props: EnvironmentSettingsProps) {
-  const [username, setUsername] = useState('')
+  const username = useContext(UsernameContext);
 
   const { loading, data, error } = useQuery(GROUPS, {
     variables: { username },
@@ -116,34 +117,6 @@ function EnvironmentSettings(props: EnvironmentSettingsProps) {
             </Grid>
             <Grid item xs={12} sm={4} md={3} textAlign={{ sm: 'right' }}>
               <Box pr={3} pb={3} display='flex' justifyContent='flex-end'>
-                Username:
-              </Box>
-            </Grid>
-            <Grid item xs={12} sm={8} md={9} pb={3}>
-              <TextField
-                id='username-field'
-                variant='standard'
-                value={username}
-                onChange={(e) => setUsername((e.target as HTMLInputElement).value)}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <Tooltip title={"Enter your username"}>
-                        <HelpOutlineIcon
-                          sx={{
-                            color: 'rgba(34, 51, 84, 0.7)',
-                            padding: '0 0 0 8px',
-                            fontSize: '25px'
-                          }}
-                        />
-                      </Tooltip>
-                    </InputAdornment>
-                  ),
-                }}>
-              </TextField>
-            </Grid>
-            <Grid item xs={12} sm={4} md={3} textAlign={{ sm: 'right' }}>
-              <Box pr={3} pb={3} display='flex' justifyContent='flex-end'>
                 Folder:
               </Box>
             </Grid>
@@ -157,9 +130,11 @@ function EnvironmentSettings(props: EnvironmentSettingsProps) {
                     props.setPath(e.target.value);
                   }}
                 >
-                  {(data?.groups?.length ?? 0) > 0
-                    ? <MenuItem value={`users/${username}`}>users/{username}</MenuItem>
-                    : <MenuItem disabled>Invalid username</MenuItem>}
+                  {username
+                    ? (data?.groups?.length ?? 0) > 0
+                      ? <MenuItem value={`users/${username}`}>users/{username}</MenuItem>
+                      : <MenuItem disabled>Invalid username</MenuItem>
+                    : <MenuItem disabled>Enter your username in the top-right</MenuItem>}
                   {loading || error ? null : data?.groups.map(({ name }: { name: string }) =>
                     <MenuItem key={name} value={`groups/${name}`}>groups/{name}</MenuItem>)}
                   {error && <MenuItem disabled>error getting groups: {error.message}</MenuItem>}
--- src/routes/Root/index.tsx
+++ src/routes/Root/index.tsx
@@ -3,24 +3,53 @@ import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 import { Outlet } from 'react-router-dom';
 
 import Sidebar from '../../components/Sidebar';
+import { InputAdornment, TextField, Tooltip } from '@mui/material';
+import { useState } from 'react';
+import { UsernameContext } from '../../components/UsernameContext';
 
 const Root = () => {
+  const [username, setUsername] = useState("");
+
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
       <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }} color="inherit">
         <Toolbar>
-          <Typography variant="h4">SoftPack</Typography>
+          <Typography variant="h4" sx={{ flexGrow: 1 }}>SoftPack</Typography>
+          <Typography pr={1}>Username:</Typography>
+          <TextField
+            variant='standard'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <Tooltip title={"Enter your username"}>
+                    <HelpOutlineIcon
+                      sx={{
+                        color: 'rgba(34, 51, 84, 0.7)',
+                        padding: '0 0 0 8px',
+                        fontSize: '25px'
+                      }}
+                    />
+                  </Tooltip>
+                </InputAdornment>
+              ),
+            }}>
+          </TextField>
         </Toolbar>
       </AppBar>
-      <Sidebar />
-      <Box component="main" sx={{ mx: 2, width: "100%" }}>
-        <Toolbar />
-        <Outlet />
-      </Box>
+      <UsernameContext.Provider value={username}>
+        <Sidebar />
+        <Box component="main" sx={{ mx: 2, width: "100%" }}>
+          <Toolbar />
+          <Outlet />
+        </Box>
+      </UsernameContext.Provider>
     </Box>
   );
 };
