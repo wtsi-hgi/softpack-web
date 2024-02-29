9cda07130fa7cd5e92335b5341eae95c6504acf7
Sendu Bala <sb10@sanger.ac.uk>
2024-02-29 14:04:33 +0000

    Make vite port configurable.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
+import { ConfigEnv, defineConfig, loadEnv } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({command, mode} : ConfigEnv) => {
+    const cwd = process.cwd();
+    const env = {...loadEnv(mode, cwd, 'VITE_')};
+
+    // reusable config for both server and preview
+    const serverConfig = {
+        host: true,
+        port: Number(env.VITE_PORT?? 5173),
+        strictPort: true,
+    };
+
+    return {
+        base: '/',
+        plugins: [react()],
+        preview: serverConfig,
+        server: serverConfig,
+    };
 });
