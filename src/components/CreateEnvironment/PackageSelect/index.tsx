d81798354f6732c2c364bbbe431a6e0e8120897c
Iaroslav Popov <ip13@sanger.ac.uk>
2024-03-21 12:15:22 +0000

    Warn about missing packages from clone, WIP

--- src/components/CreateEnvironment/PackageSelect/index.tsx
+++ src/components/CreateEnvironment/PackageSelect/index.tsx
@@ -1,4 +1,4 @@
-import { Autocomplete, Box, TextField } from "@mui/material";
+import { Alert, Autocomplete, Box, TextField } from "@mui/material";
 import { ReactNode, useMemo } from "react";
 
 import { Package } from "../../../queries";
@@ -15,12 +15,32 @@ type PackageSelectParams = {
 // Displays an autocomplete box, where the option(s) selected are MUI chips,
 // each with their own dropdown to display package versions.
 export default function PackageSelect(props: PackageSelectParams) {
+  const validSelectedPackages: Package[] = []
+  const invalidSelectedPackages: Package[] = []
+  const invalidSelectedVersionPackages: Package[] = []
+
+  props.selectedPackages.forEach((pkg) => {
+    const envPkgVersions = props.packages.get(pkg.name)
+    const validPkg: Package = { name: pkg.name, version: pkg.version }
+
+    // TODO move to function with docstring
+    if (envPkgVersions) {
+      if (pkg.version && !(pkg.version in envPkgVersions)) {
+        invalidSelectedVersionPackages.push(pkg)
+        validPkg.version = envPkgVersions[0]
+      }
+      validSelectedPackages.push(validPkg)
+    } else {
+      invalidSelectedPackages.push(pkg)
+    }
+  })
+
   const selectedPackageNames = useMemo(
-    () => props.selectedPackages.map(({ name }) => name),
+    () => validSelectedPackages.map(({ name }) => name),
     [props.selectedPackages],
   );
   const selectedPackageVersions = useMemo(
-    () => props.selectedPackages.map(({ version }) => version),
+    () => validSelectedPackages.map(({ version }) => version),
     [props.selectedPackages],
   );
 
@@ -98,6 +118,20 @@ export default function PackageSelect(props: PackageSelectParams) {
           );
         }}
       />
+
+      {invalidSelectedPackages.length > 0 ? (
+        <Alert severity="error">
+          These packages no longer exist: {invalidSelectedPackages.map(x => x.name).join(", ")}
+        </Alert>
+      ) : null}
+
+      {
+        invalidSelectedVersionPackages.length > 0 ? (
+          <Alert severity="warning">
+            These package versions no longer exist (the latest has been used): {invalidSelectedVersionPackages.map(x => x.name + "@" + x.version).join(", ")}
+          </Alert>
+        ) : null
+      }
     </Box>
   );
 }
