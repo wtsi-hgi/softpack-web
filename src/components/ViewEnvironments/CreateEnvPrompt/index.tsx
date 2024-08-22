a2568d90be5f992f3cb8d03f14319555a102760d
Daniel Elia <de7@sanger.ac.uk>
2024-08-22 09:59:06 +0100

    Create new environment with package if package not found (#106)
    * Add box-shadow when hovering over environment

---
+++ src/components/ViewEnvironments/CreateEnvPrompt/index.tsx
@@ -0,0 +1,47 @@
+import { Box, Chip, Typography } from "@mui/material"
+import { useLocalStorage } from "@uidotdev/usehooks";
+import { NavLink } from "react-router-dom";
+import { Package } from "../../../queries";
+
+export default function CreateEnvPrompt({ name, pkgs }: { name: string, pkgs: string[] }) {
+	const [packages, setSelectedPackages] = useLocalStorage<Package[]>("environments-selectedpackages", []);
+	const packageMatch: string[] = [];
+	pkgs.forEach((p: string) => {
+		if (p.includes(name)) {
+			packageMatch.push(p);
+		}
+	});
+
+	const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+		const p = packages;
+		const names = new Set((packages ?? []).map(e => e.name));
+		const newPackage: Package = { name: e.currentTarget.textContent as string };
+		if (!names.has(newPackage.name)) {
+			p.push(newPackage);
+		}
+		setSelectedPackages(p);
+	};
+
+	return (
+		<Box textAlign="center">
+			<Typography variant="h2">No environments found...</Typography>
+			{packageMatch.length > 0 &&
+				<>
+					<Typography variant="body1">Create one by selecting from the following packages:</Typography>
+					{packageMatch.map((val: string) =>
+						<>
+							<Chip
+								key={val}
+								label={val}
+								style={{ margin: "4px" }}
+								onClick={handleClick}
+								component={NavLink as React.ElementType}
+								to={"/create"}
+							/>
+						</>
+					)}
+				</>
+			}
+		</Box>
+	)
+}
