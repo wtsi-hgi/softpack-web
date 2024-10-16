18e66dcdedf4e0ff7c58df8f44ac9d3f31afe073
Michael Woolnough <130465766+mjkw31@users.noreply.github.com>
2024-10-16 12:27:21 +0100

    Add warning when version is added to environment name. (#116)
    * Add ToolTip to explain that versions are managed by SoftPack.

--- src/components/HelpIcon/index.tsx
+++ src/components/HelpIcon/index.tsx
@@ -3,11 +3,12 @@ import { Tooltip } from "@mui/material";
 
 export type HelpIconProps = {
   title: string;
+  style?: React.CSSProperties;
 };
 
 export const HelpIcon = (props: HelpIconProps) => {
   return (
-    <Tooltip title={props.title}>
+    <Tooltip style={props.style ?? {}} title={props.title}>
       <HelpOutlineIcon
         sx={{
           color: "rgba(34, 51, 84, 0.7)",
