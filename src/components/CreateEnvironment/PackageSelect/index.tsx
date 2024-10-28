b308bd8302650b420054e993bee2b9e9046505ca
Michael Woolnough <mw31@sanger.ac.uk>
2024-10-28 12:06:29 +0000

    Re-corrected use of memo's.

--- src/components/CreateEnvironment/PackageSelect/index.tsx
+++ src/components/CreateEnvironment/PackageSelect/index.tsx
@@ -17,31 +17,11 @@ type PackageSelectParams = {
 // Displays an autocomplete box, where the option(s) selected are MUI chips,
 // each with their own dropdown to display package versions.
 export default function PackageSelect(props: PackageSelectParams) {
-  const validPackages: Package[] = []
-  const invalidSelectedPackages: Package[] = []
-  const invalidSelectedVersionPackages: Package[] = []
-  const [recipeFilter, setRecipeFilter] = useState("");
-
-  useMemo(
-    () => {
-      validPackages.length = 0;
-      const validated = validatePackages(props.selectedPackages, props.packages)
-      validPackages.push(...validated[0])
-      invalidSelectedPackages.push(...validated[1])
-      invalidSelectedVersionPackages.push(...validated[2])
-    },
-    [props.selectedPackages],
-  );
-
-  const selectedPackageNames = useMemo(
-    () => validPackages.map(({ name }) => name),
-    [validPackages],
-  );
-  const selectedPackageVersions = useMemo(
-    () => validPackages.map(({ version }) => version),
-    [validPackages],
-  );
-  const [recipeDescriptions, getRecipeDescription] = useContext(recipeDescriptionContext);
+  const [recipeFilter, setRecipeFilter] = useState(""),
+        [validPackages, invalidSelectedPackages, invalidSelectedVersionPackages] = useMemo(() => validatePackages(props.selectedPackages, props.packages), [props.selectedPackages]),
+        selectedPackageNames = useMemo(() => validPackages.map(({ name }) => name), [validPackages]),
+        selectedPackageVersions = useMemo(() => validPackages.map(({ version }) => version), [validPackages]),
+        [recipeDescriptions, getRecipeDescription] = useContext(recipeDescriptionContext);
 
   // renderTags displays each selected autocomplete option as an MUI chip which
   // contains a dropdown, hence the custom name, DropdownChip.
